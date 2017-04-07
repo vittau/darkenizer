818595db7d47fbc46c6e3eb7f6625ac3f32287d1
Vitor Machado <vitor.p.mach@gmail.com>
2017-04-07 20:53:58 -0300

    Transition

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var Vue = require('vue/dist/vue');
 var app = new Vue({
 	el: '#app',
 	data: {
+		step: 1,
 		message: 'Paste your SCSS file here...'
+	},
+
+	methods: {
+		parseSCSS: function() {
+			this.step = 2;
+		}
 	}
-})
+});
